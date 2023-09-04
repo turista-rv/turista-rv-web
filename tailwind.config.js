54ba370a8a89fe7459be1e143e796e002e5a7397
Felipe Silva <127339296+iamfelipeS@users.noreply.github.com>
2023-09-04 16:43:36 -0300

    tailwind add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,52 @@
 module.exports = {
   content: ["./**/*.html", "./**/*.ts"],
   theme: {
-    extend: {},
+    extend: {
+
+      fontFamily: {
+        roboto: ['Roboto', 'sans'],
+      },
+
+      colors: {
+        blueDark: '#0D7ABF',
+        blueMid: '#118FCF',
+        blueLight: '#1397D6',
+
+        greenDark: '#2BA245',
+        greenMid: '#CBE4B4',
+        greenLight: '#E5F0DB',
+
+        //BASE
+        grays1: '#1B1D1E',
+        grays2: '#333638',
+        grays3: '#5C6265',
+        grays4: '#B9BBBC',
+        
+
+        grays5: '#DDDEDF',
+        grays6: '#EFF0F0',
+        grays7: '#FAFAFA',
+        white: '#FFFFFF',
+
+      },
+
+      fontSize: {
+        '12': '12px',
+        '14': '14px',
+        '16': '16px',
+        '18': '18px',
+        '24': '24px',
+        '32': '32px',
+      },
+
+      width: {
+        '314': '314px',
+      },
+
+      height: {
+        '55': '55px',
+      },
+    },
   },
   plugins: [],
 }
