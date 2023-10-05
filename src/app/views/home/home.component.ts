4da2a698a3a39b6325455f09711b1b7d886ddebe
Felipe Silva <127339296+iamfelipeS@users.noreply.github.com>
2023-10-05 13:45:03 -0300

    home changes

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -6,10 +6,10 @@ import { Component, AfterViewInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent  {
+export class HomeComponent {
   searchTerm: string = ''; // Variável para armazenar o termo de pesquisa
   cards: any[] = [/* Seus dados de cards aqui */];
- 
+
   isDropdownVisible: boolean = false;
 
   toggleDropdown(): void {
@@ -27,4 +27,28 @@ export class HomeComponent  {
   handleDateClick(arg: any) {
     alert('Date clicked: ' + arg.dateStr);
   }
+
+  activeTab: number = 0; // A aba ativa começa do índice 0
+  tabs: any[] = [
+    {
+      title: 'Tab 1',
+      content: 'Conteúdo da Tab 1'
+    },
+    {
+      title: 'Tab 2',
+      content: 'Conteúdo da Tab 2'
+    },
+    {
+      title: 'Tab 3',
+      content: 'Conteúdo da Tab 3'
+    },
+    {
+      title: 'Tab 4',
+      content: 'Conteúdo da Tab 4'
+    },
+    {
+      title: 'Tab 5',
+      content: 'Conteúdo da Tab 5'
+    }
+  ];
 }
