9130b53956e5e627379f04d493861f76cf9d850e
Felipe Silva <127339296+iamfelipeS@users.noreply.github.com>
2023-10-10 08:44:12 -0300

    login, logout and register methods add

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,24 +1,87 @@
+import { api } from './../../api';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs'; 
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  // private apiUrl = 'https://api-olharlaser.onrender.com/api/users';
-  private apiUrl = 'https://api-turistarv-0xio.onrender.com/api/users';
+  url = api.url + '/users';
+  token: string | null;
+  
 
-  constructor(private http: HttpClient) {}
+  private _isLoggedIn: boolean = false;
+  
+  constructor(private http: HttpClient) {
+    this.token = localStorage.getItem('token');
+  }
+
+  loginUser(email: string, password: string): Observable<boolean> {
+    return this.http
+      .post<any>(this.url, { email: email, password: password })
+      .pipe(
+        map((data: any) => {
+          if (data && data.token && data.user) {
+            this.setToken(data.token);
+            localStorage.setItem('idUser', data.user.id.toString());
+            localStorage.setItem('isAdmin', data.user.admin.toString());
+            return true;
+          }
+          throw new Error('Resposta inválida do servidor');
+        }),
+        catchError((error: any) => {
+          alert('Erro durante o login: ' + error.message);
+          throw error; 
+        })
+      );
+  }
 
   registerUser(user: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}`, user);
-   
+    return this.http.post<any>(this.url, user).pipe(
+      map((response: any) => response), // Mapeia a resposta para retorná-la diretamente
+      catchError((error: any) => {
+        alert(error);
+        console.error('Erro ao registrar usuário:', error);
+        throw error; 
+      })
+    );
   }
 
-  loginUser(credentials: { email: string, password: string }): Observable<any> {
-    
-    return this.http.post(`${this.apiUrl}/login`, credentials);
-    console.log()
+  logout(refreshToken: string): Observable<any> {
+    return this.http.post<any>(this.url, { refreshToken }).pipe(
+      catchError((error: any) => {
+        console.error('Erro durante o logout:', error);
+        throw error; 
+      })
+    );
   }
+
+  getToken(): string {
+    return this.token as string;
+  }
+
+  setToken(token: string): void {
+    localStorage.setItem('token', token);
+    this.token = token;
+  }
+
+  clearToken(): void {
+    localStorage.removeItem('token');
+    this.token = null;
+  }
+
+  setLoggedIn(): void {
+    this._isLoggedIn = true;
+  }
+
+  setLoggedOut(): void {
+    this._isLoggedIn = false;
+  }
+
+  isLoggedIn(): boolean {
+    return this._isLoggedIn;
+  }
+  
 }
