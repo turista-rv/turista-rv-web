dc26f442d23e0758b8ef86417ff6769ec66958e5
Felipe Silva <127339296+iamfelipeS@users.noreply.github.com>
2023-10-10 08:46:49 -0300

    isLoggedIn changes

--- src/app/template/navbar/navbar.component.ts
+++ src/app/template/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
-// navbar.component.ts
+import { Router } from '@angular/router';
+import { AuthService } from './../../services/auth.service';
 import { Component, ElementRef, Renderer2 } from '@angular/core';
+
 
 @Component({
   selector: 'app-navbar',
@@ -9,7 +11,16 @@ import { Component, ElementRef, Renderer2 } from '@angular/core';
 export class NavbarComponent {
   isMenuOpen = false;
 
-  constructor(private renderer: Renderer2, private el: ElementRef) {}
+  constructor(
+    private renderer: Renderer2,
+    private el: ElementRef,
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
@@ -23,4 +34,26 @@ export class NavbarComponent {
       this.renderer.removeClass(menuElement, 'navbar-below-header');
     }
   }
+  logout(): void {
+    const refreshToken = localStorage.getItem('refreshToken') as string;
+    this.authService.logout(refreshToken).subscribe(
+      (data: any) => {
+      console.log(data);
+      localStorage.removeItem('token');
+      localStorage.removeItem('refreshToken');
+      localStorage.removeItem('idUser');
+      localStorage.removeItem('isAdmin');
+      this.router.navigateByUrl('/login');
+    });
+   
+    this.router.navigateByUrl('/login');
+  }
 }
+
+// localStorage.removeItem('token');
+// localStorage.removeItem('refreshToken');
+// this.isLoggedIn = false; 
+
+// const x = localStorage.getItem('refreshToken')
+// console.log(x)
+// console.log('Tokens removidos: ', this.isLoggedIn);
