14b239ff40ad18a92579e9433370d9d835c02634
Felipe Silva <127339296+iamfelipeS@users.noreply.github.com>
2023-10-10 08:46:30 -0300

    interceptor and api.ts add

---
+++ src/app/services/http-interceptor.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpInterceptorService implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    // Obtém o token de autenticação do AuthService
+    const authToken = this.authService.getToken();
+
+    // Clona a requisição original e adiciona o token ao header
+    const authReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    });
+
+    // Encaminha a requisição clonada com o token para o próximo manipulador no pipeline
+    return next.handle(authReq);
+  }
+
+  
+}
