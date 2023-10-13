ce4e05fc933a35b95a012a8b9d192841191ccfcc
Felipe Silva <127339296+iamfelipeS@users.noreply.github.com>
2023-10-13 11:34:50 -0300

    reset-password add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ResetPasswordComponent } from './views/reset-password/reset-password.component';
 import { AdminPanelComponent } from './views/admin-panel/admin-panel.component';
 import { HostessComponent } from './views/hostess/hostess.component';
 import { CreateAccountComponent } from './views/create-account/create-account.component';
@@ -7,6 +8,7 @@ import { HomeComponent } from "./views/home/home.component";
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './views/about/about.component';
+
 const routes: Routes = [
   {
     path: "",
@@ -19,6 +21,10 @@ const routes: Routes = [
   {
     path: "create-account",
     component: CreateAccountComponent,
+  },
+  {
+    path: "reset-password",
+    component: ResetPasswordComponent,
   },
   {
     path: "about",
