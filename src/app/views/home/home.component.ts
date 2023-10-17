2448cdb39445f8af00c7a010ca7466a2bf619821
Felipe Silva <127339296+iamfelipeS@users.noreply.github.com>
2023-10-17 18:45:31 -0300

    responsive changes

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -77,7 +77,11 @@ export class HomeComponent {
         {
           imageFileName: 'pomerode.jpg',
           imageTitle: 'Título da Terceira Imagem'
-        }
+        },
+        // {
+        //   imageFileName: 'pomerode.jpg',
+        //   imageTitle: 'Título da Terceira Imagem'
+        // },
       ]
     },
     {
