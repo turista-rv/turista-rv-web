4d276ac8c75742bf7ef4d1609b5c611eab74cf3b
Felipe Silva <127339296+iamfelipeS@users.noreply.github.com>
2023-10-30 23:48:17 -0300

    login with key ENTER

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,7 +1,7 @@
+import { Component, ElementRef, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 import { map } from 'rxjs/operators';
 import { LoginUser } from './../../models/LoginUser.model';
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -14,29 +14,35 @@ export class LoginComponent {
     email: '',
     password: '',
   };
-  
+
   isLoggedIn: boolean = false;
 
   constructor(
     private authService: AuthService,
-    private router: Router
-  ) { }
+    private router: Router,
+    private elementRef: ElementRef
+  ) {}
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent): void {
+    if (event.key === 'Enter' && (event.target as HTMLElement).tagName !== 'BUTTON') {
+      event.preventDefault();
+      this.onSubmit();
+    }
+  }
 
   onSubmit() {
-    console.log(this.credentials.email, this.credentials.password)
     this.authService
       .loginUser(this.credentials.email, this.credentials.password)
       .pipe(map((data: LoginUser) => {
-        localStorage.setItem('token', data.accessToken)
-        
+        localStorage.setItem('token', data.accessToken);
       }))
       .subscribe((data: any) => {
         this.isLoggedIn = true;
         this.router.navigateByUrl('/');
       },
-        (error: any) => {
-          console.error('Erro durante o login:', error);
-        }
-      );
+      (error: any) => {
+        console.error('Erro durante o login:', error);
+      });
   }
 }
