24774ceda3ecff10783a2d8f712c0412d0b68f52
Felipe Silva <127339296+iamfelipeS@users.noreply.github.com>
2023-11-14 16:01:25 -0300

    checkout template styles

--- src/app/views/camping-checkout/camping-checkout.component.ts
+++ src/app/views/camping-checkout/camping-checkout.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { CampingService } from 'src/app/services/camping.service';
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-camping-checkout',
   templateUrl: './camping-checkout.component.html',
   styleUrls: ['./camping-checkout.component.css']
 })
-export class CampingCheckoutComponent {
+export class CampingCheckoutComponent implements OnInit {
+  campingName: string | null = null;
+  campingPrice: number | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private CampingService: CampingService
+  ) {}
+
+   ngOnInit(): void {
+  //   this.route.queryParams.subscribe(params => {
+  //     this.campingName = params['name'];
+  //     this.campingPrice = params['price'];
+  //   });
+   }
+
+  finalizarReserva() {
+
+    console.log('Reserva finalizada para o camping:', this.campingName);
+    console.log('Preço do camping:', this.campingPrice);
+  }
 }
