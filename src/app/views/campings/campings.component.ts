dc7a22e0e90a77df063893095df87b71609d11fc
Gustavo Reimche <gustavo.reimche@linx.com.br>
2023-11-16 01:48:03 -0300

    ajuste no datepicker

--- src/app/views/campings/campings.component.ts
+++ src/app/views/campings/campings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { Camping } from 'src/app/models/camping.model';
@@ -12,8 +13,15 @@ import { CampingService } from 'src/app/services/camping.service';
 export class CampingsComponent implements OnInit {
   camping!: Camping;
   id: string | null = null;
+  dataEntrada: Date = new Date();
+  dataSaida: Date = new Date();
 
   public Editor = ClassicEditor;
+
+  range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null),
+  });
 
   constructor(
     private route: ActivatedRoute,
@@ -31,4 +39,29 @@ export class CampingsComponent implements OnInit {
         this.camping = camping[0];
       });
   }
+
+  sendMessageWhatsapp() {
+    const range = this.range.value;
+    if (range.start && range.end) {
+      const dataIni = `${range.start.getDate()}/${
+        range.start.getMonth() + 1
+      }/${range.start.getFullYear()}`;
+      const dataFim = `${range.end.getDate()}/${
+        range.end.getMonth() + 1
+      }/${range.end.getFullYear()}`;
+      const mensagemWhatsapp = `Olá, gostaria de fazer uma reserva para de ${dataIni} até ${dataFim}!`;
+
+      window.open(
+        `http://wa.me/5553999356737?text=${encodeURIComponent(
+          mensagemWhatsapp
+        )}`,
+        '_blank'
+      );
+    }
+  }
+
+  myFilter = (d: Date | null): boolean => {
+    const currentDate = new Date();
+    return !d || d >= currentDate;
+  };
 }
